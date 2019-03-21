4674d1019d7cda1cd925cc57990e04bdb121a930
Alex Gayle <46231238+Alex-Gayle@users.noreply.github.com>
2019-03-20 19:45:40 -0500

    ojij

--- CSCI-413-Java-Project/src/Main.java
+++ CSCI-413-Java-Project/src/Main.java
@@ -934,7 +934,7 @@ public class Main extends JFrame {
 				
 			}
 			catch (Exception e) {
-				JOptionPane.showMessageDialog(null, e.getStackTrace()[0].getLineNumber());
+				JOptionPane.showMessageDialog(null, e);
 			}
 		}
 	}
